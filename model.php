6e7dacb799910dc12042bdc6565c4ea65623756a
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-03 03:49:15 +0400

    Update model.php
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- model.php
+++ model.php
@@ -11,17 +11,7 @@ if ($mysqli->connect_error) {
     die("Ошибка подключения к базе данных: " . $mysqli->connect_error);
 }
 
-// Функция для отправки SMS через Gammu
-function sendSMSWithGammu($port, $phoneNumber, $message) {
-    $command = "gammu-smsd-inject TEXT $phoneNumber -text \"$message\" -unicode -device $port";
-    exec($command, $output, $returnVar);
 
-    if ($returnVar === 0) {
-        return "SMS отправлено успешно";
-    } else {
-        return "Ошибка при отправке SMS";
-    }
-}
 
 // Функция для сохранения сообщения в базе данных
 function saveMessage($modemPort, $phoneNumber, $message) {
