f2598e3dc23bfaa1130e2d846174f9e08d603559
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-03 03:11:41 +0400

    Update index.php
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
-
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <!-- HTML form -->
 <!DOCTYPE html>
 <html>
