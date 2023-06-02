4425a952b1736ae8a6d563d7432c71b7e73b2e8b
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-03 03:23:40 +0400

    Update index.php
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <!-- HTML form -->
 <!DOCTYPE html>
 <html>
 <head>
     <title>Send SMS</title>
     <meta charset="utf-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    
 </head>
 <body>
     <h1>Send SMS</h1>
