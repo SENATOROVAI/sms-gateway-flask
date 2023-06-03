b81d921e04bf91117f349b9599d7d3076efe8f0a
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-03 17:02:44 +0400

    Update index.php
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- index.php
+++ index.php
@@ -4,22 +4,33 @@
     <title>Send SMS</title>
     <meta charset="utf-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    
 </head>
 <body>
     <h1>Send SMS</h1>
 
+    <table>
+        <tr>
+            <th>Modem Port</th>
+            <th>Status</th>
+        </tr>
+        <?php foreach ($modemPorts as $modemPort): ?>
+            <tr>
+                <td><?php echo $modemPort; ?></td>
+                <td><?php echo isset($modemStatuses[$modemPort]) ? $modemStatuses[$modemPort] : "Unknown"; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
     <form method="POST" action="controller.php">
         <label>Enter recipient phone numbers (one number per line):</label><br>
         <textarea name="phone_numbers" rows="4" cols="50"></textarea><br><br>
 
         <label>Enter message:</label><br>
         <textarea name="message" rows="4" cols="50"></textarea><br><br>
-
         <input type="submit" value="Send">
     </form>
 </body>
 </html>
+
 
             
             
