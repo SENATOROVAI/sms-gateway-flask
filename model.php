5ce4410e4c7dc7b46943b35959fb505af2bf6cf6
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-03 18:21:41 +0400

    Update model.php
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- model.php
+++ model.php
@@ -5,22 +5,22 @@ $username = "root";
 $password = "";
 $database = "SMSGATE";
 
-$mysqli = new mysqli($host, $username, $password, $database);
-
-if ($mysqli->connect_error) {
-    die("Ошибка подключения к базе данных: " . $mysqli->connect_error);
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Ошибка подключения к базе данных: " . $e->getMessage());
 }
-
-
 
 // Функция для сохранения сообщения в базе данных
 function saveMessage($modemPort, $phoneNumber, $message) {
-    global $mysqli;
+    global $pdo;
 
     $sql = "INSERT INTO messages (modem_port, phone_number, message) VALUES (:modem_port, :phone_number, :message)";
-    $statement = $mysqli->prepare($sql);
-    $statement->bindParam(':modem_port', $modem_port);
-    $statement->bindParam(':phone_number', $phone_number);
+    $statement = $pdo->prepare($sql);
+    $statement->bindParam(':modem_port', $modemPort);
+    $statement->bindParam(':phone_number', $phoneNumber);
     $statement->bindParam(':message', $message);
     $statement->execute();
 }
+?>
