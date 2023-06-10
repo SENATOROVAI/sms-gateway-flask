54cd4ed55d01f3473a32a4d74d2b8d964cc4d7fb
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-11 02:00:21 +0400

    add type hints
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- app/models/model.py
+++ app/models/model.py
@@ -1,21 +1,23 @@
 import os
 from dotenv import load_dotenv
 import mysql.connector
+from typing import Optional, Tuple, List, Dict
 
 load_dotenv()
 
 
 class Database:
-    def __init__(self):
+    def __init__(self) -> None:
         self.connection = mysql.connector.connect(
-            host=os.getenv("DATABASE_HOST"),
-            user=os.getenv("DATABASE_USER"),
-            password=os.getenv("DATABASE_PASSWORD"),
-            database=os.getenv("DATABASE_NAME"),
+            host=os.getenv('DATABASE_HOST'),
+            user=os.getenv('DATABASE_USER'),
+            password=os.getenv('DATABASE_PASSWORD'),
+            database=os.getenv('DATABASE_NAME')
         )
 
-    def execute_query(self, query, params=None):
-        result = None
+    def execute_query(self, query: str, 
+                      params: Optional[Tuple] = None) -> Optional[List[Tuple]]:
+        result: Optional[List[Tuple]] = None
         try:
             connection = self.connection
             with connection.cursor() as cursor:
@@ -33,19 +35,19 @@ class Database:
 
 class MessageLogModel:
     @staticmethod
-    def save_message(modem_port, phone_number, message, status):
-        db = Database()
-        query = "INSERT INTO `logs` (`id`, `imei`, `number`, `sms`, `status`) VALUES (NULL, %s, %s, %s, %s);"
+    def save_message(modem_port: str, phone_number: str, message: str, status: int) -> None:
+        db: Database = Database()
+        query: str = "INSERT INTO `logs` (`id`, `imei`, `number`, `sms`, `status`) VALUES (NULL, %s, %s, %s, %s);"
         db.execute_query(query, (modem_port, phone_number, message, status))
 
     @staticmethod
-    def get_data():
-        db = Database()
-        query = "SELECT * FROM logs;"
-        return db.execute_query(query)
+    def get_data() -> Optional[List[Tuple]]:
+        db: Database = Database()
+        query: str = "SELECT * FROM logs;"
+        return db.execute_query(query) 
 
     @staticmethod
-    def delete_messages():
-        db = Database()
-        query = "DELETE FROM logs;"
+    def delete_messages() -> None:
+        db: Database = Database()
+        query: str = "DELETE FROM logs;"
         db.execute_query(query)
