1915c71769151cdb3352fdada6a60aec5ce94e49
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-11 00:07:24 +0400

    Update controller.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,4 +1,5 @@
 import subprocess
+
 # import random
 from itertools import cycle
 
@@ -32,13 +33,24 @@ class SMSController:
             "COM17": "gammurc6",
             "COM17": "gammurc7",
             "COM25": "gammurc8",
-            "COM25": "gammurc9"
+            "COM25": "gammurc9",
         }
         return modem_ports
 
     def send_sms(self):
         list_num = []
-        numbers = [11231,1231231,21323123,1222312,12131,2131231,2111,2313,123123,123123,1231,1231,12312,12312,12312,12312,12312,12312,12312,123123,123123,21312,12312,123123,1231321,1231231,12312313,1231321,1231312,1231312,123131231,12313132131,13123]
+        numbers = [
+            11231,
+            1231231,
+            21323123,
+            1222312,
+            12131,
+            2131231,
+            2111,
+            2313,
+            123123,
+            123123,
+        ]
         modem_ports = {
             "COM17": "gammurc",
             "COM11": "gammurc1",
@@ -49,60 +61,63 @@ class SMSController:
             "COM17-2": "gammurc6",
             "COM17-3": "gammurc7",
             "COM25": "gammurc8",
-            "COM25-1": "gammurc9"
+            "COM25-1": "gammurc9",
         }
 
         modem_index = 0  # переменная для отслеживания текущего индекса модема
 
         for number in numbers:
-            modem_port = list(modem_ports)[modem_index % len(modem_ports)]  # выбор модема по текущему индексу
+            modem_port = list(modem_ports)[
+                modem_index % len(modem_ports)
+            ]  # выбор модема по текущему индексу
             modem_index += 1  # увеличение индекса модема на 1
-            list_num.append([number, [modem_port,"-",modem_ports[modem_port]]])
+            list_num.append([number, [modem_port, "-", modem_ports[modem_port]]])
         return list_num
 
         get_data = self.message_log_model.get_data()
-        if request.method == 'POST':
-            phone_numbers = request.form.get('phone_numbers')
-            message = request.form.get('message')
+        if request.method == "POST":
+            phone_numbers = request.form.get("phone_numbers")
+            message = request.form.get("message")
 
             if phone_numbers and message:
                 for phone_number in phone_numbers.split():
                     random_modem_port = self.get_random_modem_port()
-                    response = self.send_sms_command(random_modem_port, phone_number, message)
-                    self.message_log_model.save_message(random_modem_port, phone_number, message, response)
+                    response = self.send_sms_command(
+                        random_modem_port, phone_number, message
+                    )
+                    self.message_log_model.save_message(
+                        random_modem_port, phone_number, message, response
+                    )
 
-                return render_template('index.html', get_data=get_data, response=response)
+                return render_template(
+                    "index.html", get_data=get_data, response=response
+                )
 
-        return render_template('index.html', get_data=get_data, response=response)
+        return render_template("index.html", get_data=get_data, response=response)
 
     def get_data(self):
         get_data = self.message_log_model.get_data()
-        return render_template('index.html', get_data=get_data)
-    
+        return render_template("index.html", get_data=get_data)
 
     def download_csv(self):
-            result = self.message_log_model.get_data()
+        result = self.message_log_model.get_data()
 
-            fieldnames = ['id', 'imei', 'number', 'sms', 'status'] 
+        fieldnames = ["id", "imei", "number", "sms", "status"]
 
-            csv_data = StringIO()
-            writer = csv.DictWriter(csv_data, fieldnames=fieldnames)
+        csv_data = StringIO()
+        writer = csv.DictWriter(csv_data, fieldnames=fieldnames)
 
-            writer.writeheader() 
+        writer.writeheader()
 
-            for row in result:
-                writer.writerow(dict(zip(fieldnames, row)))
+        for row in result:
+            writer.writerow(dict(zip(fieldnames, row)))
 
-            response = Response(csv_data.getvalue(), mimetype='text/csv')
-            response.headers.set('Content-Disposition', 'attachment', filename='data.csv')
+        response = Response(csv_data.getvalue(), mimetype="text/csv")
+        response.headers.set("Content-Disposition", "attachment", filename="data.csv")
 
-            return response
-    
+        return response
 
     def delete_send_sms(self):
         self.message_log_model.delete_messages()
         delete_button = "Таблица успешно удалена"
-        return render_template('index.html', delete_button=delete_button)
-
-
-
+        return render_template("index.html", delete_button=delete_button)
