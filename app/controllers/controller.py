2083d9ec0926151f5fcda98ce02dbd08f9e34818
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-11 01:43:52 +0400

    Update controller.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -8,19 +8,19 @@ from io import StringIO
 
 
 class SMSController:
-    def __init__(self):
-        self.message_log_model = MessageLogModel()
+    def __init__(self) -> None:
+        self.message_log_model: MessageLogModel = MessageLogModel()
 
-    def send_sms_command(self, gammurc, phone_number, message):
-        command = f'C:\\Gammu\\bin\\gammu -c C:\\Gammu\\bin\\{gammurc} sendsms TEXT {phone_number} -text "{message}"'
+    def send_sms_command(self, gammurc:srt, phone_number:str, message:str)->str:
+        command:str = f'C:\\Gammu\\bin\\gammu -c C:\\Gammu\\bin\\{gammurc} sendsms TEXT {phone_number} -text "{message}"'
         try:
             subprocess.check_output(command, shell=True)
             return 1
         except subprocess.CalledProcessError as e:
             return f" {e.output.decode('utf-8').strip()}"
 
-    def get_random_modem_port(self):
-        modem_ports = {
+    def get_random_modem_port(self)->dict:
+        modem_ports:dict = {
             "COM17": "gammurc",
             "COM11": "gammurc1",
             "COM13": "gammurc2",
@@ -34,9 +34,9 @@ class SMSController:
         }
         return modem_ports
 
-    def send_sms(self):
-        list_num = []
-        numbers = [
+    def send_sms(self)->list:
+        list_num: list = []
+        numbers: list = [
             11231,
             1231231,
             21323123,
@@ -48,7 +48,7 @@ class SMSController:
             123123,
             123123,
         ]
-        modem_ports = {
+        modem_ports: dict = {
             "COM17": "gammurc",
             "COM11": "gammurc1",
             "COM13": "gammurc2",
@@ -61,10 +61,10 @@ class SMSController:
             "COM25-1": "gammurc9",
         }
 
-        modem_index = 0  # переменная для отслеживания текущего индекса модема
+        modem_index: int = 0  # переменная для отслеживания текущего индекса модема
 
         for number in numbers:
-            modem_port = list(modem_ports)[
+            modem_port: str = list(modem_ports)[
                 modem_index % len(modem_ports)
             ]  # выбор модема по текущему индексу
             modem_index += 1  # увеличение индекса модема на 1
@@ -92,14 +92,14 @@ class SMSController:
 
         return render_template("index.html", get_data=get_data, response=response)
 
-    def get_data(self):
+    def get_data(self)-str:
         get_data = self.message_log_model.get_data()
         return render_template("index.html", get_data=get_data)
 
-    def download_csv(self):
+    def download_csv(self)->Response:
         result = self.message_log_model.get_data()
 
-        fieldnames = ["id", "imei", "number", "sms", "status"]
+        fieldnames: list = ["id", "imei", "number", "sms", "status"]
 
         csv_data = StringIO()
         writer = csv.DictWriter(csv_data, fieldnames=fieldnames)
@@ -114,7 +114,7 @@ class SMSController:
 
         return response
 
-    def delete_send_sms(self):
+    def delete_send_sms(self)->str:
         self.message_log_model.delete_messages()
-        delete_button = "Таблица успешно удалена"
+        delete_button: str = "Таблица успешно удалена"
         return render_template("index.html", delete_button=delete_button)
