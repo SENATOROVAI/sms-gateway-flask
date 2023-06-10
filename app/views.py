d602e3235f9228d5c157a165ab31cf2e38f0a295
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-11 00:00:16 +0400

    Update views.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- app/views.py
+++ app/views.py
@@ -4,26 +4,28 @@ from controllers.controller import SMSController
 app = Flask(__name__)
 controller = SMSController()
 
-@app.route('/repeat', methods=['GET', 'POST'])
+
+@app.route("/repeat", methods=["GET", "POST"])
 def repeat_send_sms():
     return "repeat"
 
 
-@app.route('/delete', methods=['GET', 'POST'])
+@app.route("/delete", methods=["GET", "POST"])
 def delete_send_sms():
     return controller.delete_send_sms()
 
 
-@app.route('/send', methods=['GET', 'POST'])
+@app.route("/send", methods=["GET", "POST"])
 def send_sms():
     return controller.send_sms()
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def main():
     return controller.get_data()
 
-@app.route('/get', methods=['GET', 'POST'])
+
+@app.route("/get", methods=["GET", "POST"])
 def get_db():
     return controller.download_csv()
 
