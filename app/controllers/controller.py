3ee3029366132fe4c03c4c4a632fe87050fcca8d
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-10 23:58:09 +0400

    get modem port ver 1.1

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,5 +1,6 @@
 import subprocess
-import random
+# import random
+from itertools import cycle
 
 from flask import render_template, request, Response
 from models.model import MessageLogModel
@@ -33,13 +34,36 @@ class SMSController:
             "COM25": "gammurc8",
             "COM25": "gammurc9"
         }
-        return random.choice(list(modem_ports))
+        return modem_ports
 
     def send_sms(self):
+        list_num = []
+        numbers = [11231,1231231,21323123,1222312,12131,2131231,2111,2313,123123,123123,1231,1231,12312,12312,12312,12312,12312,12312,12312,123123,123123,21312,12312,123123,1231321,1231231,12312313,1231321,1231312,1231312,123131231,12313132131,13123]
+        modem_ports = {
+            "COM17": "gammurc",
+            "COM11": "gammurc1",
+            "COM13": "gammurc2",
+            "COM14": "gammurc3",
+            "COM15": "gammurc4",
+            "COM17-1": "gammurc5",
+            "COM17-2": "gammurc6",
+            "COM17-3": "gammurc7",
+            "COM25": "gammurc8",
+            "COM25-1": "gammurc9"
+        }
+
+        modem_index = 0  # переменная для отслеживания текущего индекса модема
+
+        for number in numbers:
+            modem_port = list(modem_ports)[modem_index % len(modem_ports)]  # выбор модема по текущему индексу
+            modem_index += 1  # увеличение индекса модема на 1
+            list_num.append([number, [modem_port,"-",modem_ports[modem_port]]])
+        return list_num
+
         get_data = self.message_log_model.get_data()
         if request.method == 'POST':
-            phone_numbers = request.form.get('phone_numbers', '')
-            message = request.form.get('message', '')
+            phone_numbers = request.form.get('phone_numbers')
+            message = request.form.get('message')
 
             if phone_numbers and message:
                 for phone_number in phone_numbers.split():
