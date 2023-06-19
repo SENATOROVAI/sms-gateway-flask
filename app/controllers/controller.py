56ba7cbaec893f0bd89acc2060261267588d38dc
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-06-20 00:15:23 +0400

    Update controller.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,96 +1,93 @@
+from http.client import responses
 import subprocess
+from typing import Optional
 
-from flask import render_template, request, Response
+from flask import render_template, request, Response, redirect
 from ..models.model import MessageLogModel
 
 import csv
 from io import StringIO
+
+import asyncio
+
+import concurrent.futures
+
+# from asyncio import run
 
 
 class SMSController:
     def __init__(self) -> None:
         self.message_log_model: MessageLogModel = MessageLogModel()
 
-    def send_sms_command(self, gammurc: str, phone_number: str, message: str) -> str:
-        command: str = f'C:\\Gammu\\bin\\gammu -c C:\\Gammu\\bin\\{gammurc} sendsms TEXT {phone_number} -text "{message}"'
-        try:
-            subprocess.check_output(command, shell=True)
-            return 1
-        except subprocess.CalledProcessError as e:
-            return f" {e.output.decode('utf-8').strip()}"
-
     def get_random_modem_port(self) -> dict:
         modem_ports: dict = {
-            "COM17": "gammurc",
-            "COM11": "gammurc1",
-            "COM13": "gammurc2",
-            "COM14": "gammurc3",
-            "COM15": "gammurc4",
-            "COM17": "gammurc5",
-            "COM17": "gammurc6",
-            "COM17": "gammurc7",
-            "COM25": "gammurc8",
-            "COM25": "gammurc9",
+            "COM1": "gammurc",
+            "COM2": "gammurc1",
+            # "COM3": "gammurc2",
+            "COM4": "gammurc3",
+            "COM5": "gammurc4",
+            # "COM6": "gammurc5",
+            # "COM7": "gammurc6",
+            # "COM8": "gammurc7",
+            # "COM9": "gammurc8",
+            # "COM10": "gammurc9",
         }
         return modem_ports
 
-    def send_sms(self) -> list:
-        list_num: list = []
-        numbers: list = [
-            11231,
-            1231231,
-            21323123,
-            1222312,
-            12131,
-            2131231,
-            2111,
-            2313,
-            123123,
-            123123,
-        ]
-        modem_ports: dict = {
-            "COM17": "gammurc",
-            "COM11": "gammurc1",
-            "COM13": "gammurc2",
-            "COM14": "gammurc3",
-            "COM15": "gammurc4",
-            "COM17-1": "gammurc5",
-            "COM17-2": "gammurc6",
-            "COM17-3": "gammurc7",
-            "COM25": "gammurc8",
-            "COM25-1": "gammurc9",
-        }
+    def send_sms_command(self, gammurc: str, phone_number: str, message: str) -> str:
+        command: str = f'C:\\Gammu\\bin\\gammu -c C:\\Gammu\\bin\\{gammurc} sendsms TEXT {phone_number} -len 400 -unicode -text "{message}"'
+        process = subprocess.run(command, shell=True, capture_output=True, text=True)
 
+        if process.returncode == 0:
+            return "ok"
+        else:
+            return process.stdout.strip()
+
+
+    def send_sms(self) -> list:
+        data_to_csv: List = []
         modem_index: int = 0  # переменная для отслеживания текущего индекса модема
 
-        for number in numbers:
-            modem_port: str = list(modem_ports)[
-                modem_index % len(modem_ports)
-            ]  # выбор модема по текущему индексу
-            modem_index += 1  # увеличение индекса модема на 1
-            list_num.append([number, [modem_port, "-", modem_ports[modem_port]]])
-        return list_num
-
-        get_data = self.message_log_model.get_data()
         if request.method == "POST":
             phone_numbers = request.form.get("phone_numbers")
             message = request.form.get("message")
 
             if phone_numbers and message:
-                for phone_number in phone_numbers.split():
-                    random_modem_port = self.get_random_modem_port()
-                    response = self.send_sms_command(
-                        random_modem_port, phone_number, message
-                    )
-                    self.message_log_model.save_message(
-                        random_modem_port, phone_number, message, response
-                    )
+                with concurrent.futures.ThreadPoolExecutor() as executor:
+                
+                    for phone_number in phone_numbers.split():
+                        modem_port: str = list(self.get_random_modem_port())[
+                            modem_index % len(self.get_random_modem_port())
+                        ]
+                        modem_index += 1
 
-                return render_template(
-                    "index.html", get_data=get_data, response=response
-                )
+                        future = executor.submit(
+                            self.send_sms_command,
+                            self.get_random_modem_port()[modem_port],
+                            phone_number,
+                            message,
+                        )
+                       
 
-        return render_template("index.html", get_data=get_data, response=response)
+                        data_to_csv.append(
+                            {
+                                "modem_port": modem_port,
+                                "phone_number": phone_number,
+                                "message": message,
+                                "response": future.result(),
+                            }
+                        )
+
+        for data in data_to_csv:
+            modem_port = data["modem_port"]
+            phone_number = data["phone_number"]
+            message = data["message"]
+            response = data["response"]
+            self.message_log_model.save_message(
+                modem_port, phone_number, message, response
+            )
+
+            # return redirect("/")
 
     def get_data(self) -> str:
         get_data = self.message_log_model.get_data()
@@ -116,5 +113,5 @@ class SMSController:
 
     def delete_send_sms(self) -> str:
         self.message_log_model.delete_messages()
-        delete_button: str = "Таблица успешно удалена"
-        return render_template("index.html", delete_button=delete_button)
+        # delete_button: str = "Таблица успешно удалена"
+        return redirect("/")
